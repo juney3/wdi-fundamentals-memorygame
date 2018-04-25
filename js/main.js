e42941267e88b68288da0951eeefba005add2399
juney3 <juney37@gmail.com>
2018-04-25 16:04:43 -0700

    added game logic

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Make sure script file is correctly linked
+console.log("Up and running!");
+
+//Declare variables for card names
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+//Display value of the two cards that the user flipped over
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
